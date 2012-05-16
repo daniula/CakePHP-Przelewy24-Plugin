d25383ed52766e90117d20103a0829bfe4548f41
Daniel Mendalka <daniel.mendalka@gmail.com>
2012-05-16 21:19:31 +0200

    handling settings and some refactoring (removing 'p24_' prefixes from code)

--- View/Helper/P24Helper.php
+++ View/Helper/P24Helper.php
@@ -1,26 +1,51 @@
 <?PHP
 App::uses('FormHelper', 'View/Helper');
+App::uses('Router', 'Routing');
 
 class P24Helper extends FormHelper {
-  private $options = null;
+  private $settings = array();
+  private $settingsFields = array(
+    'session_id',
+    'id_sprzedawcy',
+    'klient',
+    'adres',
+    'kod',
+    'miasto',
+    'kraj',
+    'email',
+    'language',
+    'kwota',
+    'return_url_ok',
+    'return_url_error',
+    'metoda',
+    'opis',
+  );
 
   private function getSubmitUrl() {
     return 'https://secure.przelewy24.pl/index.php';
   }
 
-  private function parseData($data = null) {
-    $result = array();
-
-    foreach ($data as $field => $value) {
-
-    }
-
-    return $result;
+  public function __construct(View $view, $settings = array()) {
+      parent::__construct($view, $settings);
+      $this->setSettings($settings);
   }
 
-  public function create($data = null, $options = array()) {
-    if ($data !== null) {
-      $this->parseData($data);
+  public function setSettings($settings = array()) {
+    foreach ($settings as $field => $value) {
+      if (in_array($field, $this->settingsFields)) {
+        if ( in_array($field, array('return_url_error', 'return_url_ok')) && is_array($value) ) {
+          $value = Router::url($value, true);
+        }
+        $this->settings[$field] = $value;
+      }
+    }
+
+    return $this->settings;
+  }
+
+  public function create($settings = null, $options = array()) {
+    if ($settings !== null) {
+      $this->setSettings($settings);
     }
     if (!isset($options['url'])) {
       $options['url'] = $this->getSubmitUrl();
@@ -34,40 +59,32 @@ class P24Helper extends FormHelper {
     }
 
     if (!isset($options['value']) &&
-        isset($this->data[str_replace('p24_', '', $fieldName)])
+        isset($this->options[$fieldName])
     ) {
-      $options['value'] = $this->data[str_replace('p24_', '', $fieldName)];
+      $options['value'] = $this->settings[$fieldName];
     }
 
-    return parent::hidden($fieldName, $options);
+    return parent::hidden('p24_'.$fieldName, $options);
   }
 
-  public function data($newData = null) {
-    if ($newData === null) {
-      return $data;
-    } else {
-      $data = $this->parseData($newData);
-    }
-  }
-
-  public function session($data = null) {
+  public function session($options = null) {
     $result = array();
 
-    $result[] = $this->hidden('p24_session_id');
-    $result[] = $this->hidden('p24_id_sprzedawcy');
+    $result[] = $this->hidden('session_id');
+    $result[] = $this->hidden('id_sprzedawcy');
 
     return join("\n", $result);
   }
 
   public function price() {
-    return $this->hidden('p24_kwota');
+    return $this->hidden('kwota');
   }
 
   public function callbackUrls() {
     $result = array();
 
-    $result[] = $this->hidden('p24_return_url_ok');
-    $result[] = $this->hidden('p24_return_url_error');
+    $result[] = $this->hidden('return_url_ok');
+    $result[] = $this->hidden('return_url_error');
 
     return join("\n", $result);
   }
@@ -79,39 +96,40 @@ class P24Helper extends FormHelper {
         $result[] = $this->Html->useTag('javascriptlink', 'https://secure.przelewy24.pl/external/formy.php?id=9722&amp;sort=2', '');
         $result[] = $this->Html->useTag('javascriptblock', 'm_formy();', '');
     } else {
-        $result[] = $this->hidden('p24_metoda');
+        $result[] = $this->hidden('metoda');
     }
 
     return join("\n", $result);
   }
 
-  public function description($data = null) {
-    $result[] = $this->hidden('p24_opis');
-    if(Configure::read() > 0) {
-       //'TEST_OK';
+  public function description($settings = null) {
+    if(Configure::read() == 0) {
+       $result[] = $this->hidden('opis');
+    } else {
+      $result[] = $this->hidden('opis', array('value' => 'TEST_OK'));
     }
   }
 
   public function crc() {
-    return $this->hidden('p24_crc');
+    return $this->hidden('crc');
   }
 
-  public function optionalFields($data = null) {
+  public function optionalFields($settings = null) {
     $result = array();
 
-    $result[] = $this->hidden('p24_klient');
-    $result[] = $this->hidden('p24_adres');
-    $result[] = $this->hidden('p24_kod');
-    $result[] = $this->hidden('p24_miasto');
-    $result[] = $this->hidden('p24_kraj');
-    $result[] = $this->hidden('p24_email');
+    $result[] = $this->hidden('klient');
+    $result[] = $this->hidden('adres');
+    $result[] = $this->hidden('kod');
+    $result[] = $this->hidden('miasto');
+    $result[] = $this->hidden('kraj');
+    $result[] = $this->hidden('email');
 
-    $result[] = $this->hidden('p24_language');
+    $result[] = $this->hidden('language');
 
     return join("\n", $result);
   }
 
-  public function requiredFields($data = null) {
+  public function requiredFields($settings = null) {
     $result = array();
     $result[] = $this->session();
     $result[] = $this->price();
@@ -122,7 +140,7 @@ class P24Helper extends FormHelper {
     return join("\n", $result);
   }
 
-  public function allFields($data = null) {
+  public function allFields($settings = null) {
     $result = array();
     $result[] = $this->requiredFields();
     $result[] = $this->optionalFields();
